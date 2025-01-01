1204232022df66e5502e91553f95735bb9dd0592
Sridhar S M <sridharsmwork@gmail.com>
2025-01-01 22:12:44 +0530

    changes for form
    Signed-off-by: Sridhar S M <sridharsmwork@gmail.com>

--- iPortfolio/script.js
+++ iPortfolio/script.js
@@ -4,3 +4,30 @@ function downloadResume() {
     link.download = 'Sridhar_Resume.pdf';
     link.click();
 }
+
+const form = document.querySelector('.inputForm');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    const formObject = Object.fromEntries(formData.entries());
+
+    try {
+        const response = await fetch('https://sridhars-webapp-restapi-postgres-v2.onrender.com', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(formObject),
+        });
+
+        const result = await response.json();
+        if (response.ok) {
+            alert(result.message);
+        } else {
+            alert('Error: ' + result.message);
+        }
+    } catch (error) {
+        console.error('Error submitting form:', error);
+        alert('An error occurred while submitting the form.');
+    }
+});
